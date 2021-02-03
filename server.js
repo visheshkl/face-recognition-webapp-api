35de900404957822fcbab50b43fb423c158f5c30
Vishesh Khandelwal <visheshkkl@gmail.com>
2021-02-03 14:55:42 +0530

    updated backend without db

--- server.js
+++ server.js
@@ -1,11 +1,86 @@
 const express=require('express');
+const bodyParser=require('body-parser');
+const cors=require('cors');
 
 const app=express();
+app.use(bodyParser.json());
+app.use(cors());
 
+const database ={
+    users:[
+        {
+            id:'123',
+            name:'John',
+            password:'cookies',
+            email:'john@gmail.com',
+            entries:0,
+            joined:new Date()
+        },
+        {
+            id:'124',
+            name:'Sally',
+            password:'bananas', 
+            entries:0,
+            joined:new Date()
+        }
+    ],
+}
 app.get('/',(req,res)=>{
-    res.send("This is working");
+    res.json(database.users);
 })
 
-app.listen(3000,()=>{
-    console.log('app is running on port 3000');
+app.post('/signin',(req,res)=>{
+    if(req.body.email===database.users[0].email && req.body.password === database.users[0].password){
+        res.json('Success');
+    }else{
+        res.status(400).json('Error Logging In');
+    }
+})
+
+app.post('/register',(req,res)=>{
+    const {email,name,password}=req.body
+    database.users.push({
+        id:'125',
+        name:name,
+        email:email,
+        entries:0,
+        joined:new Date()
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id',(req,res)=>{
+    const {id}=req.params;
+    let found=false;
+    database.users.forEach(user=>{
+        if(user.id===id){
+            found=true;
+            return res.json(user);
+        }
+    })
+    if(found===false)
+    {
+        res.status(400).json('No Such User');
+    }
+})
+
+app.put('/image',(req,res)=>{
+    const {id}=req.body;
+    let found=false;
+    database.users.forEach(user=>{
+        if(user.id===id){
+            found=true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if(found===false)
+    {
+        res.status(400).json('Not Found');
+    }
+})
+
+
+app.listen(3001,()=>{
+    console.log('app is running on port 3001');
 })
